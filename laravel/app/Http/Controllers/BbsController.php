d1ac7f0040784ece207a1663d1e4925fce178a97
Plotnost <zoo1102@mail.ru>
2023-11-08 17:50:41 +0500

    Получение сведений о текущем пользователе

--- laravel/app/Http/Controllers/BbsController.php
+++ laravel/app/Http/Controllers/BbsController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Models\Bb; // Перемещено сюда
 
 class BbsController extends Controller {
@@ -19,4 +18,3 @@ class BbsController extends Controller {
     }
 
 }
-
--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -51,3 +51,5 @@ class User extends Authenticatable
 
     }
 }
+
+
